16da3109973fc0d7950f045004fa3985eede9624
3klikDeveloper <krz.andrukiewicz@gmail.com>
2023-10-06 15:30:13 +0200

    Reverting changes on useInView on mobile

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,24 +1,15 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useActiveSectionContext } from "@context/active-section-context"
 import { useInView } from "react-intersection-observer"
 import { SectionName } from "./types"
 import { useRouter } from "next/navigation"
 
-export function useSectionInView(
-  sectionName: SectionName,
-  desktopThreshold = 0.5,
-  mobileThreshold = 0.1
-) {
-  const [threshold, setThreshold] = useState(desktopThreshold)
+export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
   const { ref, inView } = useInView({
-    threshold: threshold,
+    threshold,
   })
   const { setActiveSection, timeOfLastClick } = useActiveSectionContext()
   const router = useRouter()
-
-  useEffect(() => {
-    setThreshold(window.innerWidth < 600 ? mobileThreshold : desktopThreshold)
-  }, [desktopThreshold, mobileThreshold])
 
   useEffect(() => {
     if (inView && Date.now() - timeOfLastClick > 1000) {
