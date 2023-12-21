d384cbbae77821a45a98e485470fb1e9d49ba3e9
Andrukiewicz <krz.andrukiewicz@gmail.com>
2023-12-21 05:12:31 +0100

    Cafe project added to project list

--- lib/data.ts
+++ lib/data.ts
@@ -1,10 +1,7 @@
-import React from "react"
-import { CgWorkAlt } from "react-icons/cg"
-import { FaReact } from "react-icons/fa"
-import { LuGraduationCap } from "react-icons/lu"
 import klikImg from "@public/klik.png"
 import eloImg from "@public/elopokrowce_small.png"
 import kanbanImg from "@public/trollo_small.png"
+import coffeepandaImg from "@public/coffeepanda.webp"
 
 export const links = [
   {
@@ -33,39 +30,22 @@ export const links = [
   },
 ] as const
 
-export const experiencesData = [
-  {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
-    description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
-  },
-] as const
-
 export const projectsData = [
+  {
+    title: "Café web design",
+    time: "December 2023",
+    description: "Beautiful, animated website idea for your Café.",
+    tags: ["Next.js", "React", "Tailwind", "Framer motion"],
+    available: true,
+    siteUrl: "https://coffeepanda.vercel.app/",
+    codeUrl: "https://github.com/Andrukiewicz/CoffeePanda",
+    imageUrl: coffeepandaImg,
+  },
   {
     title: "Must have Todo App",
     time: "October 2023",
     description: "Come on. Everyone has to have To Do app in portfolio.",
-    tags: ["React", "Next.js", "Tailwind", "Beautiful-dnd"],
+    tags: ["Next.js", "React", "Tailwind", "Beautiful-dnd"],
     available: true,
     siteUrl: "https://trollo-self.vercel.app/",
     codeUrl: "https://github.com/Andrukiewicz/todolist",
